7703986ab4358f7c1763b74a5f61bafe04b420a0
Ammar051 <ammarbahriang@gmail.com>
2021-02-14 15:51:10 +0500

    changed loginactions

--- LoanApprovalPredictor/urls.py
+++ LoanApprovalPredictor/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from Register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', include("main.urls")),
     path('',include("django.contrib.auth.urls")),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views as v
 
