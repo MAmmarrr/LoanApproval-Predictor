46b6420e8aa415132d27442b91814233f179b25c
Ammar051 <ammarbahriang@gmail.com>
2021-02-15 03:53:11 +0500

    addedstatic

--- LoanApprovalPredictor/settings.py
+++ LoanApprovalPredictor/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_cdn/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static_cdn")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
