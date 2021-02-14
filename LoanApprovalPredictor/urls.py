5085fc100dfca220466eba8ca06e727c792846a9
Ammar051 <ammarbahriang@gmail.com>
2021-02-15 03:49:43 +0500

    added

--- LoanApprovalPredictor/urls.py
+++ LoanApprovalPredictor/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from django.contrib import admin
 from django.urls import path, include
@@ -10,4 +13,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', include("main.urls")),
     path('',include("django.contrib.auth.urls")),
-]
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
